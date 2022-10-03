555095656da695daf6edfac588d076a26216641d
licco01 <jvfssantana2018@gmail.com>
2022-10-03 19:07:23 -0300

    initial commit

---
+++ src/main/java/com/br/licco01/aplicacao_java_spring/AplicacaoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.br.licco01.aplicacao_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/licco01/aplicacao_java_spring/AplicacaoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.licco01.aplicacao_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
