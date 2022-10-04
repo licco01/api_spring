20abe6fdbefdfd94784e9878a1fc628132679120
licco01 <jvfssantana2018@gmail.com>
2022-10-03 21:40:04 -0300

    criação das entidades da aplicação e da mapeação de cada uma para nossa base de dados

---
+++ src/main/java/com/br/licco01/aplicacao_java_spring/entity/Lancamento.java
@@ -0,0 +1,117 @@
+package com.br.licco01.aplicacao_java_spring.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.br.licco01.aplicacao_java_spring.enums.TipoEnum;
+
+@Entity
+@Table(name = "lancamento")
+public class Lancamento implements Serializable {
+
+	private static final long serialVersionUID = -5549408315400382328L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data", nullable = false)
+	private Date data;
+	
+	@Column(name = "descricao", nullable = true)
+	private String descricao;
+	
+	@Column(name = "localizacao", nullable = true)
+	private String localizacao;
+	
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataCriacao;
+	
+	@Column(name = "data_atualizacao", nullable = false)
+	private Date dataAtualizacao;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo", nullable = false)
+	private TipoEnum tipoEnum;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Funcionario funcionario;
+	
+	public Lancamento() {
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getLocalizacao() {
+		return localizacao;
+	}
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+	public TipoEnum getTipoEnum() {
+		return tipoEnum;
+	}
+	public void setTipoEnum(TipoEnum tipoEnum) {
+		this.tipoEnum = tipoEnum;
+	}
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	@Override
+	public String toString() {
+		return "Lancamento [id=" + id + ", data=" + data + ", descricao=" + descricao + ", localizacao=" + localizacao
+				+ ", dataCriacao=" + dataCriacao + ", dataAtualizacao=" + dataAtualizacao + ", tipoEnum=" + tipoEnum
+				+ ", funcionario=" + funcionario + "]";
+	}
+	
+}
---
+++ src/main/java/com/br/licco01/aplicacao_java_spring/enums/PerfilEnum.java
@@ -0,0 +1,8 @@
+package com.br.licco01.aplicacao_java_spring.enums;
+
+public enum PerfilEnum {
+
+	ROLE_ADMIN,
+	ROLE_USUARIO
+	
+}
